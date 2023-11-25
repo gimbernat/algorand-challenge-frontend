30073add62f6c271115e0d79dd2e51b7b4c867bc
Diego <gimbernat13@gmail.com>
2023-11-25 13:59:36 -0500

    experimental animation update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+/* Your custom styles go here */
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
